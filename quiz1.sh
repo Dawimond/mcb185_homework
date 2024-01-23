26799bb5d2cdc3260ef7273f1211874c163c315e
username <mantodea@campus-007-201.ucdavis.edu>
2024-01-23 09:12:41 -0800

    quiz1

--- quiz1.sh
+++ quiz1.sh
@@ -2,3 +2,9 @@ echo "user: $USER"
 echo "name: Yutong Ji"
 
 gunzip -c dictionary.gz | grep -v "[^muocatf]" | grep "a" | grep -E "[muocatf].{2}[muocatf]" | wc
+
+gunzip -c dictionary.gz | grep -v "[^taibrnl]" | grep "b" | grep -E "[taibrnl]].{2}[taibrnl]" | wc
+
+gunzip -c dictionary.gz | grep -v "[^maocdin]" | grep "c" | grep -E "[maocdin].{2}[maocdin]" | wc
+
+gunzip -c dictionary.gz | grep -v "[^anozigr]" | grep "z" | grep -E "[anozigr].{2}[anozigr]" | wc
