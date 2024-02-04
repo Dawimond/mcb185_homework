f302c1485668dd7c1b7fa6116a0b9e7cca9715be
username <mantodea@Yutongs-MacBook-Air.local>
2024-02-03 21:31:06 -0800

    up

--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def pythagoras(a, b): 
-	if a <= 0: sys.exit('error: a must be greater than 0')
-	if b <= 0: sys.exit('error: b must be greater than 0')
-	return math.sqrt(a**2 + b**2)
-x = pythagoras(-3, -4)
-print(x)
